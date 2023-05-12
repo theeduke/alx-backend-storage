4adefc4fde1fd4914b0b31ad14601d6cd267e861
theeduke <oscarian528@gmail.com>
2023-05-12 08:14:21 -0700

    inserting document using python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+insert a document in python
+
+"""
+import pymongo
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert document into a collection
+
+    """
+    data = mongo_collection.insert(kwargs)
+    return data.inserted_id
